0109c83cacbccd2911f39d4bc5b278cac3c80042
XhmikosR <xhmikosr@gmail.com>
2017-11-05 13:40:17 +0200

    Try out cssnano.

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,12 +1,23 @@
 'use strict'
 
-module.exports = (ctx) => ({
-  map: ctx.file.dirname.includes('examples') ? false : {
-    inline: false,
-    annotation: true,
-    sourcesContent: true
-  },
-  plugins: {
-    autoprefixer: { cascade: false }
+module.exports = (ctx) => {
+  const cssnanoOptions = {
+    preset: ['default', {
+      mergeRules: false,
+      minifyFontValues: false,
+      reduceTransforms: false
+    }]
   }
-})
+
+  return {
+    map: ctx.file.dirname.includes('examples') ? false : {
+      inline: false,
+      annotation: true,
+      sourcesContent: true
+    },
+    plugins: {
+      autoprefixer: { cascade: false },
+      cssnano: ctx.env === 'production' ? cssnanoOptions : false
+    }
+  }
+}
